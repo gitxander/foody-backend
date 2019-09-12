51722d51a9f429e4a575016e21de28b3f35b3db2
Xander Lopez <xanderlopez@me.com>
2019-09-12 14:05:40 +1000

    fixes for order api

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -69,18 +69,24 @@ class OrderController extends Controller
     {
         if($request->isMethod('put') && $request->has('User_Id'))
         {
-            $cart = app('db')->select("SELECT * FROM carts WHERE carts.checkout = 0 AND carts.user_id = $request->input('User_Id')");
+            $user_id = $request->input('User_Id');
+            $cart = app('db')->select("SELECT * FROM carts WHERE carts.checkout = 0 AND carts.user_id = $user_id");
+
+            if(count($cart) == 0)
+            {
+                return response()->json(array('No Pending Order'));
+            }
 
             $total = 0;
             $order_id = null;
             foreach ($cart as $key => $value) {
-                $total += $value['total'];
-                $order_id = $value['order_id']
+                $total += $value->total;
+                $order_id = $value->order_id;
             }
 
             $results = app('db')->select(
                 "UPDATE carts SET
-                checkout = 1
+                checkout = '1'
                 WHERE order_id = $order_id");
 
             $results = app('db')->select(
@@ -88,7 +94,7 @@ class OrderController extends Controller
                 total = '$total',
                 checkout = 1
                 WHERE id = $order_id");
-                
+
             return response()->json($results);
         }
     }
